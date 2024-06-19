23cc94f6146766fd9320d2193f9e5320ec9f1476
it-s23029 <s23029@std.it-college.ac.jp>
2024-06-19 11:26:16 +0900

    データクラス

---
+++ src/main/kotlin/jp/ac/it_college/std/s23029/messageboard/domain/model/Message.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s23029.messageboard.domain.model
+
+import kotlinx.datetime.LocalDateTime
+
+data class Message(
+    val id: Long,
+    val threadId: Long,
+    val userId: Long,
+    val message: String,
+    val postedAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+    val deleted: Boolean
+)
---
+++ src/main/kotlin/jp/ac/it_college/std/s23029/messageboard/domain/model/Threads.kt
@@ -0,0 +1,12 @@
+package jp.ac.it_college.std.s23029.messageboard.domain.model
+
+import kotlinx.datetime.LocalDateTime
+
+data class Threads(
+    val id: Long,
+    val title: String,
+    val userId: Long,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+    val deleted: Boolean
+)
