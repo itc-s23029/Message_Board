16937924a3478c6e747adeab39672e6977eb8e8a
it-s23029 <s23029@std.it-college.ac.jp>
2024-06-27 09:42:59 +0900

    実装環境

--- src/main/kotlin/jp/ac/it_college/std/s23029/messageboard/presentation/controller/ThreadsController.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23029/messageboard/presentation/controller/ThreadsController.kt
@@ -1,4 +1,22 @@
 package jp.ac.it_college.std.s23029.messageboard.presentation.controller
 
-class ThreadsController {
+import jp.ac.it_college.std.s23029.messageboard.application.service.ThreadsService
+import jp.ac.it_college.std.s23029.messageboard.presentation.form.GetThreadListResponse
+import jp.ac.it_college.std.s23029.messageboard.presentation.form.ThreadsInfo
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/threads")
+@CrossOrigin
+class ThreadsController (
+    private val threadsService: ThreadsService,
+) {
+    @GetMapping("/list")
+    fun getList(): GetThreadListResponse {
+        val threadsList = threadsService.getList().map(::ThreadsInfo)
+        return GetThreadListResponse(threadsList)
+    }
 }
